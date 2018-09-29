5f3e67a50c021c97908d053f55c32efed30237d1
Joseph Cwiek <juzcruizinalong@gmail.com>
2018-09-29 15:16:05 +0930

    Main menu script, shooting, projectiles working and scoremanager.

---
+++ Nature V Technology/Assets/Scripts/CameraActor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraActor : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public Transform target;
+    public Vector3 offset;
+
+    private Vector3 boom;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get the vector from the target to us
+        boom = this.transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set our position to be the same relative to the player
+        Vector3 target_pos = target.position + boom + offset;
+        this.transform.position = Vector3.Lerp(transform.position, target_pos, speed * Time.deltaTime);
+    }
+}
---
+++ Nature V Technology/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    // The player's score.
+    public int score;
+
+    // Reference to the Text component.
+    public Text text;
+
+    void Awake()
+    {
+        // Set up the reference.
+        //text = GetComponent<Text>();
+
+        // Reset the score.
+        score = 0;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set the displayed text to be the word "Score" followed by the score value.
+        text.text = "ROBOTS DESTROYED: " + score;
+    }
+}
