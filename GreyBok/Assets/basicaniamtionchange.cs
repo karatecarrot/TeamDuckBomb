8105e7e5ccf2e19589e170a52c8935a5457879c3
Jay Miller <jayswork9@gmail.com>
2018-09-29 14:08:58 +0930

    updated and organised minibot. Animated the medium bot.

---
+++ GreyBok/Assets/basicaniamtionchange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicaniamtionchange : MonoBehaviour
+{
+    public Animator minibot;
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(Minibot());
+	}
+	
+    IEnumerator Minibot()
+    {
+        yield return new WaitForSeconds(10);
+        minibot.SetBool("Attacking", true);
+    }
+
+}
