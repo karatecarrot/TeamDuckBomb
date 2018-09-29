7574f205f81e93868ce2c49c9ffef6bd06cf74a6
Jay Miller <jayswork9@gmail.com>
2018-09-29 21:46:31 +0930

    added collectibles, and other coding stuff

---
+++ Keyboard Worrior Updated/Assets/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 2;
+
+    private void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speed);
+
+        if(GetComponent<LineRenderer>().GetPosition(1) != new Vector3(0, 0, 3))
+            GetComponent<LineRenderer>().SetPosition(1, new Vector3(0, 0, Mathf.Lerp(0, 3, speed)));
+    }
+}
---
+++ Keyboard Worrior Updated/Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public float energy = 50;
+    public float health = 100;
+
+    [Header("UI")]
+    public Slider healthSlider;
+    public Slider energySlider;
+    public float sliderFillTime = 0.1f;
+
+    private void Update()
+    {
+        energy = Mathf.Clamp(energy, 0, 100);
+        health = Mathf.Clamp(health, 0, 100);
+
+        healthSlider.value = Mathf.Lerp(healthSlider.value, health, sliderFillTime);
+        energySlider.value = Mathf.Lerp(energySlider.value, energy, sliderFillTime);
+    }
+
+    public void AddAmount (float amount, Pickup.PickupType pickupType)
+    {
+        switch (pickupType)
+        {
+            case Pickup.PickupType.health:
+                health += amount;
+                break;
+
+            case Pickup.PickupType.Battery:
+                energy += amount;
+                break;
+        }
+    }
+}
